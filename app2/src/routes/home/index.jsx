cef651423e17295162459e2cc316959ed64834bf
Sergiu Lucutar <Sergiu.Lucutar@endava.com>
2019-08-12 17:00:37 +0300

    Add second app init commit

---
+++ app2/src/routes/home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './home.scss';
+
+export default class Home extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <main>Home</main>
+    );
+  }
+}
