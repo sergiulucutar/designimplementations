b80e978f81e382015c3402ad4412204888e76050
unknown <sergiu.lucutar@gmail.com>
2019-08-29 21:31:28 +0300

    Implement the main componnet

--- app2/src/routes/home/index.jsx
+++ app2/src/routes/home/index.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import './home.scss';
+import "./home.scss";
 
 export default class Home extends React.Component {
-
   constructor() {
     super();
   }
 
   render() {
     return (
-      <main>Home</main>
+      <section class="stories">
+        <main>
+          <header>
+            <h1>MISSING</h1>
+            <h1>PIECE</h1>
+            <span>> > ></span>
+          </header>
+          <div class="splitter" />
+          <div class="story_week" />
+          <div class="hero_banner">
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
+          </div>
+        </main>
+      </section>
     );
   }
 }
