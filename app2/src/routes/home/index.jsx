68010b6e2aa8f726ddd2f9cd02b60be65ebb4268
Sergiu Lucutar <Sergiu.Lucutar@endava.com>
2019-08-30 16:30:11 +0300

    Align scroll banners and add some animation

--- app2/src/routes/home/index.jsx
+++ app2/src/routes/home/index.jsx
@@ -9,16 +9,33 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <section class="stories">
+      <section className="stories">
         <main>
           <header>
             <h1>MISSING</h1>
             <h1>PIECE</h1>
-            <span>> > ></span>
+            <div className="scroll_banner">
+              <span>></span>
+              <span>></span>
+              <span>></span>
+            </div>
           </header>
-          <div class="splitter" />
-          <div class="story_week" />
-          <div class="hero_banner">
+          <div className="story_week_info splitter">
+            <div className="author">
+              <span>Author:</span>
+              <strong>Adrian Myoasdjja</strong>
+            </div>
+          </div>
+          <div className="story_week" />
+          <div className="hero_banner">
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
+            <span>STORY OF THE WEEK</span>
             <span>STORY OF THE WEEK</span>
             <span>STORY OF THE WEEK</span>
             <span>STORY OF THE WEEK</span>
