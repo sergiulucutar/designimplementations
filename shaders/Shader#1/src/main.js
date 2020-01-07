4fe9f9a9882f79fe9b4ff621b9b3e7ad432e35eb
sergiulucutar <sergiu.lucutar@gmail.com>
2020-01-07 22:11:17 +0200

    Add shader filter

--- shaders/Shader#1/src/main.js
+++ shaders/Shader#1/src/main.js
@@ -2,3 +2,20 @@ import "./main.scss";
 import Slider from "./slider/slider";
 
 const slider = new Slider();
+
+slider.init();
+
+const interval = 1000 / 60;
+let then = Date.now();
+let now, delta;
+function loop() {
+  // now = Date.now();
+  // delta = now - then;
+  // if (delta > interval) {
+  //   then = now - (delta % interval);
+  // }
+  slider.draw();
+  requestAnimationFrame(loop);
+}
+
+loop();
--- shaders/Shader#1/src/slider/slider.js
+++ shaders/Shader#1/src/slider/slider.js
@@ -1,13 +1,46 @@
-import * as PIXI from 'pixi.js';
+import * as PIXI from "pixi.js";
+
+const vertexShader = ``;
+const fragmentShader = `precision mediump float;uniform float time;void main() {gl_FragColor = vec4(time, 0.0, 0.0, 1.0);}`;
 
 export default class Slider {
   constructor() {
-    this.bounds = [
-      window.innerWidth,
-      window.innerHeight
-    ];
-    this.renderer = new PIXI.autoDetectRenderer(this.bounds[0], this.bounds[1]);
-    document.querySelector('#slider').appendChild(this.renderer.view);
+    this.bounds = [window.innerWidth, window.innerHeight];
+    this.renderer = new PIXI.autoDetectRenderer({
+      width: this.bounds[0],
+      height: this.bounds[1]
+    });
+    document.querySelector("#slider").appendChild(this.renderer.view);
+    this.stage = new PIXI.Container();
+
+    this.uniforms = {
+      time: {
+        type: "f",
+        value: 1.0
+      }
+    };
+
+    this.shaderFilter = new PIXI.Filter(null, fragmentShader, {
+      time: { type: "f", value: 0.4 }
+    });
+
+    this.stage.filters = [this.shaderFilter];
   }
 
+  init() {
+    const sprite = PIXI.Sprite.from("img/main_01.jpg");
+    sprite.x = 0;
+    sprite.y = 0;
+
+    this.stage.addChild(sprite);
+  }
+
+  draw() {
+    // this.stage.filters[0].uniforms.u_time.value += 0.1;
+    // this.uniforms.u_time.value += 0.1;
+    this.shaderFilter.uniforms.time = 0.5;
+    // console.log(this.stage.filters[0].uniforms.time);
+
+    this.renderer.render(this.stage);
+  }
 }
