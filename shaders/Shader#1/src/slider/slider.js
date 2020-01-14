7615aeb8befc2252cfef915f70992ace2942eded
Sergiu Lucutar <Sergiu.Lucutar@endava.com>
2020-01-14 17:11:25 +0200

    Add second shader

--- shaders/Shader#1/src/slider/slider.js
+++ shaders/Shader#1/src/slider/slider.js
@@ -3,34 +3,64 @@ import { TweenLite } from "gsap";
 import { Power4 } from "gsap/src/all";
 import { Timeline } from "gsap/src/gsap-core";
 
-const fragmentShader = `
-uniform float u_progress;
-
-varying vec2 vTextureCoord;
-uniform sampler2D uSampler;
-
-mat2 getRotM(float angle) {
-  float s = sin(angle);
-  float c = cos(angle);
-  return mat2(-c, s, s, c);
-}
-
-const float PI = 3.1415;
-const float angle = PI * 0.25;
-
-void main() {
-  vec4 fromColor = texture2D( uSampler, vTextureCoord );
-  vec4 toColor = vec4( 0.0, 0.0, 0.0, 0.0 );
-
-  vec2 dispVec = vec2(fromColor.r, fromColor.g);
-
-  vec4 disp = texture2D(uSampler, vTextureCoord - dispVec * getRotM(angle) * u_progress * 0.1);
+const fragmentShaders = [
+  `
+  uniform float u_progress;
   
-  gl_FragColor = mix(disp, toColor, u_progress);
-}`;
+  varying vec2 vTextureCoord;
+  uniform sampler2D uSampler;
+  
+  mat2 getRotM(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+    return mat2(-c, s, s, c);
+  }
+  
+  const float PI = 3.1415;
+  const float angle = PI * 0.25;
+  
+  void main() {
+    vec4 fromColor = texture2D( uSampler, vTextureCoord );
+    vec4 toColor = vec4( 0.0, 0.0, 0.0, 0.0 );
+  
+    vec2 dispVec = vec2(fromColor.r, fromColor.g);
+  
+    vec4 disp = texture2D(uSampler, vTextureCoord - dispVec * getRotM(angle) * u_progress * 0.1);
+    
+    gl_FragColor = mix(disp, toColor, u_progress);
+  }`,
+  `
+  uniform float u_progress;
+  
+  varying vec2 vTextureCoord;
+  uniform sampler2D uSampler;
+
+  const float PI = 3.1415;
+  const float angle = PI * 0.25;
+
+  mat2 getRotM(float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+    return mat2(c, -s, s, c);
+  }
+
+  void main() {
+    vec4 fromColor = texture2D( uSampler, vTextureCoord );
+    vec4 toColor = vec4( 0.0, 0.0, 0.0, 0.0 );
+  
+    vec2 divide = fract(vTextureCoord * vec2(10, 10));
+
+    vec4 disp = texture2D(uSampler, vTextureCoord + getRotM(PI) * divide * u_progress * 0.1);
+    
+    gl_FragColor = mix(disp, toColor, u_progress);
+  }
+  `
+]
 
 export default class Slider {
   constructor() {
+
+    // Setup canvas
     this.bounds = [window.innerWidth, window.innerHeight];
     this.renderer = new PIXI.autoDetectRenderer({
       width: this.bounds[0],
@@ -40,11 +70,11 @@ export default class Slider {
     });
     document.querySelector("#slider").appendChild(this.renderer.view);
     this.stage = new PIXI.Container();
-
     this.sprites = new PIXI.Container();
-
     this.stage.addChild(this.sprites);
 
+    // Slider state variables
+    this.maxSprites = 3;
     this.index = 0;
     this.timeline = null;
 
@@ -52,21 +82,16 @@ export default class Slider {
   }
 
   init() {
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= this.maxSprites; i++) {
       const sprite = PIXI.Sprite.from(`img/img${i}.jpg`);
       sprite.anchor.set(0.5);
       sprite.x = this.bounds[0] / 2;
       sprite.y = this.bounds[1] / 2;
       sprite.alpha = 0;
-      sprite.filters = [
-        new PIXI.Filter(null, fragmentShader, {
-          u_progress: 0.0
-        })
-      ];
 
       this.sprites.addChild(sprite);
     }
-
+    this.changeEffect(1);
     this.sprites.getChildAt(this.index).alpha = 1;
 
     document.addEventListener("click", () => this.handleClick());
@@ -77,7 +102,7 @@ export default class Slider {
   }
 
   handleClick() {
-    const nextSlide = (this.index + 1) % 3;
+    const nextSlide = (this.index + 1) % this.maxSprites;
     if (this.isTransitioning) {
       return;
     }
@@ -105,5 +130,14 @@ export default class Slider {
         u_progress: 0,
         ease: Power4.easeInOut
       }, 0);
+  }
+
+  // Update shader
+  changeEffect(index) {
+    for (let i = 0; i < this.maxSprites; i++) {
+      this.sprites.getChildAt(i).filters = [new PIXI.Filter(null, fragmentShaders[index], {
+        u_progress: 0.0
+      })]
+    }
   }
 }
