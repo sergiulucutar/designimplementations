55526844140c8b8614a3a0c509f73e4d30781c32
Sergiu Lucutar <Sergiu.Lucutar@endava.com>
2020-01-17 10:23:20 +0200

    Swiss design nr3

--- swiss/src/design3/main.js
+++ swiss/src/design3/main.js
@@ -2,3 +2,4 @@ import "./main.scss";
 import "../../reset.scss";
 import { TweenLite, Power4, Circ, Linear } from "gsap";
 
+setTimeout(() => document.querySelector('.poster').classList.add('show'), 10);
