4b1bae3d02b50cdff226162675cb56164e727efd
sergiulucutar <sergiu.lucutar@gmail.com>
2020-02-10 20:52:23 +0200

    Start hover displacement effect

--- anagram/src/Slider.vue
+++ anagram/src/Slider.vue
@@ -1,34 +1,91 @@
 <template>
-  <div v-on:click="nextSlide">
+  <div v-on:click="nextSlide" @mousemove="mouseMove" @mouseout="mouseOut">
     <div class="anagram_background" :data-sliderIndex="sliderIndex">
       <transition-group class="anagram_background-left" name="slideleft" tag="div">
-        <div v-show="sliderIndex==0" key="1" class="layer" style="background-image: url('https://images.unsplash.com/photo-1494625927555-6ec4433b1571?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80')"></div>
-        <div v-show="sliderIndex==1" key="2" class="layer" style="background-image: url('https://images.unsplash.com/photo-1461300617643-0aeca186c805?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')"></div>
-        <div v-show="sliderIndex==2" key="3" class="layer" style="background-image: url('https://images.unsplash.com/photo-1509398270984-356a44f8a4ae?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80')"></div>
+        <div
+          v-show="sliderIndex==0"
+          key="1"
+          class="layer"
+          :style="{transform: 'translate3d(' + this.displacement + '%, 0, 0)',  backgroundImage: 'url(https://images.unsplash.com/photo-1494625927555-6ec4433b1571?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80)' }"
+        ></div>
+        <div
+          v-show="sliderIndex==1"
+          key="2"
+          class="layer"
+          style="background-image: url('https://images.unsplash.com/photo-1461300617643-0aeca186c805?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')"
+        ></div>
+        <div
+          v-show="sliderIndex==2"
+          key="3"
+          class="layer"
+          style="background-image: url('https://images.unsplash.com/photo-1509398270984-356a44f8a4ae?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80')"
+        ></div>
       </transition-group>
       <transition-group class="anagram_background-right" name="slideright" tag="div">
-        <div v-show="sliderIndex==0" key="1" class="layer" style="background-image: url('https://images.unsplash.com/photo-1494625927555-6ec4433b1571?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80')"></div>
-        <div v-show="sliderIndex==1" key="2" class="layer" style="background-image: url('https://images.unsplash.com/photo-1461300617643-0aeca186c805?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')"></div>
-        <div v-show="sliderIndex==2" key="3" class="layer" style="background-image: url('https://images.unsplash.com/photo-1509398270984-356a44f8a4ae?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80')"></div>
+        <div
+          v-show="sliderIndex==0"
+          key="1"
+          class="layer"
+          :style="{backgroundPosition: this.displacement + '%',  backgroundImage: 'url(https://images.unsplash.com/photo-1494625927555-6ec4433b1571?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80)' }"
+        ></div>
+        <div
+          v-show="sliderIndex==1"
+          key="2"
+          class="layer"
+          style="background-image: url('https://images.unsplash.com/photo-1461300617643-0aeca186c805?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80')"
+        ></div>
+        <div
+          v-show="sliderIndex==2"
+          key="3"
+          class="layer"
+          style="background-image: url('https://images.unsplash.com/photo-1509398270984-356a44f8a4ae?ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80')"
+        ></div>
       </transition-group>
     </div>
     <main>
       <span class="flavor">we are a beer company</span>
       <header>
         <span>A</span>
-        <span>NA<span class="spin" :style="{ transform: 'rotate('+ (360 / this.sliderCount * this.sliderIndex) +'deg)'}">*</span></span>
-        <span>GRAM<span class="spin" :style="{ transform: 'rotate('+ (360 / this.sliderCount * this.sliderIndex) +'deg)'}">*</span></span>
+        <span>
+          NA
+          <span
+            class="spin"
+            :style="{ transform: 'rotate('+ (360 / this.sliderCount * this.sliderIndex) +'deg)'}"
+          >*</span>
+        </span>
+        <span>
+          GRAM
+          <span
+            class="spin"
+            :style="{ transform: 'rotate('+ (360 / this.sliderCount * this.sliderIndex) +'deg)'}"
+          >*</span>
+        </span>
       </header>
       <button class="menu_button">
         <i class="fas fa-bars"></i>
       </button>
       <!-- <button class="slide_button slide_button-prev">
         <i class="fas fa-arrow-left"></i>
-      </button> -->
+      </button>-->
       <transition-group class="slide_image" name="slideleft" tag="div">
-        <img v-show="sliderIndex==0" key="1" class="layer" src="https://images.unsplash.com/photo-1534534110269-583d430e0038?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80">
-        <img v-show="sliderIndex==1" key="2" class="layer" src="https://images.unsplash.com/photo-1534057308991-b9b3a578f1b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
-        <img v-show="sliderIndex==2" key="3" class="layer" src="https://images.unsplash.com/photo-1546498159-9a2fac87e770?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=610&q=80">
+        <img
+          v-show="sliderIndex==0"
+          key="1"
+          class="layer"
+          src="https://images.unsplash.com/photo-1534534110269-583d430e0038?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        />
+        <img
+          v-show="sliderIndex==1"
+          key="2"
+          class="layer"
+          src="https://images.unsplash.com/photo-1534057308991-b9b3a578f1b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+        />
+        <img
+          v-show="sliderIndex==2"
+          key="3"
+          class="layer"
+          src="https://images.unsplash.com/photo-1546498159-9a2fac87e770?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=610&q=80"
+        />
       </transition-group>
       <transition-group class="slide_description" name="slideright" tag="div">
         <div v-show="sliderIndex==0" key="1" class="layer">
@@ -46,7 +103,7 @@
       </transition-group>
       <!-- <button class="slide_button slide_button-next">
         <i class="fas fa-arrow-right"></i>
-      </button> -->
+      </button>-->
     </main>
   </div>
 </template>
@@ -55,6 +112,7 @@
 export default {
   data() {
     return {
+      displacement: 0,
       sliderIndex: 0,
       sliderCount: 3
     };
@@ -62,6 +120,15 @@ export default {
   methods: {
     nextSlide() {
       this.sliderIndex = (this.sliderIndex + 1) % this.sliderCount;
+    },
+    mouseMove(event) {
+      this.displacement = -Math.floor(
+        Math.abs(event.clientX - window.innerWidth / 2) / 100
+      );
+    },
+    mouseOut(event) {
+      console.log("MOUSE OUT");
+      this.displacement = 25;
     }
   }
 };
@@ -119,6 +186,9 @@ export default {
     background-size: cover;
     background-position: 25% 10%;
 
+    backface-visibility: hidden;
+    transition: all 2s ease-in-out;
+
     // opacity: 1;
   }
 
@@ -149,7 +219,6 @@ main {
   display: grid;
   grid-template-rows: repeat(4, 1fr);
   grid-template-columns: repeat(4, 1fr);
-  grid-gap: 50px;
 
   width: 100%;
   height: 100%;
@@ -193,7 +262,7 @@ main {
     .layer {
       position: absolute;
       bottom: 0;
-      right: 0;
+      right: 30px;
     }
   }
 
@@ -205,6 +274,7 @@ main {
 
     .layer {
       position: absolute;
+      left: 30px;
 
       display: flex;
       flex-direction: column;
@@ -303,7 +373,7 @@ header {
   }
 
   &.slideleft-leave-to {
-    // transform: translate3d(-100%, 0, 0);
+    transform: translate3d(-40%, 0, 0);
   }
 }
 
@@ -313,7 +383,7 @@ header {
   }
 
   &.slideright-leave-to {
-    // transform: translate3d(100%, 0, 0);
+    transform: translate3d(40%, 0, 0);
   }
 }
 
