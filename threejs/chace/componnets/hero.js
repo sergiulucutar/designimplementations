499eea7885f3892530a4072774fd779d9201934b
Sergiu Lucutar <Sergiu.Lucutar@endava.com>
2020-02-21 15:50:45 +0200

    Update hero movement and colors

--- threejs/chace/componnets/hero.js
+++ threejs/chace/componnets/hero.js
@@ -2,75 +2,70 @@ import * as THREE from 'three';
 import Utils from './utils';
 
 export default class Hero {
-    constructor() {
-        this.mesh = new THREE.Group();
+  constructor() {
+    this.position = [0, 0];
 
-        const matMain = new THREE.MeshStandardMaterial({ color: 0x434343, metalness: 0.5, roughness: 0.5 });
+    this.mesh = new THREE.Group();
 
-        // Geoms
-        const geomMain = new THREE.CylinderGeometry(3, 3, 5, 8);
+    const matRed = new THREE.MeshStandardMaterial({ color: 0xBB2A27, metalness: 0.5, roughness: 0.5 });
+    const matWhite = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0.5, roughness: 0.5 });
 
-        //Geom Tip
-        const geomTip1 = new THREE.CylinderGeometry(3, 2.5, 2, 8);
-        const geomTip2 = new THREE.CylinderGeometry(2.5, 1.5, 2, 8);
-        const geomTip3 = new THREE.CylinderGeometry(1.5, 0, 2, 8);
+    //Geoms
+    const geomMain = new THREE.CylinderGeometry(3, 3, 5, 8);
 
-        //Game Tail
-        const geomTail = new THREE.CylinderGeometry(1.5, 1.5, 2, 8);
+    //Geom Tip
+    const geomTip1 = new THREE.CylinderGeometry(3, 2.5, 2, 8);
+    const geomTip2 = new THREE.CylinderGeometry(2.5, 1.5, 2, 8);
+    const geomTip3 = new THREE.CylinderGeometry(1.5, 0, 2, 8);
 
-        //Mesh
-        const meshMain = new THREE.Mesh(geomMain, matMain);
+    //Geom Tail
+    const geomTail = new THREE.CylinderGeometry(1.5, 1.5, 2, 8);
 
-        const meshTip1 = new THREE.Mesh(geomTip1, matMain);
-        const meshTip2 = new THREE.Mesh(geomTip2, matMain);
-        const meshTip3 = new THREE.Mesh(geomTip3, matMain);
+    //Mesh
+    const meshMain = new THREE.Mesh(geomMain, matWhite);
 
-        const tailMesh = new THREE.Mesh(geomTail, matMain);
+    const meshTip1 = new THREE.Mesh(geomTip1, matRed);
+    const meshTip2 = new THREE.Mesh(geomTip2, matRed);
+    const meshTip3 = new THREE.Mesh(geomTip3, matRed);
 
-        meshTip1.position.y = -3;
-        meshTip2.position.y = -5;
-        meshTip3.position.y = -7;
+    const tailMesh = new THREE.Mesh(geomTail, matRed);
 
-        tailMesh.position.y = 2;
+    meshTip1.position.y = -3;
+    meshTip2.position.y = -5;
+    meshTip3.position.y = -7;
 
-        this.mesh.add(meshMain);
-        this.mesh.add(meshTip1);
-        this.mesh.add(meshTip2);
-        this.mesh.add(meshTip3);
+    tailMesh.position.y = 2;
 
-        this.mesh.add(tailMesh);
+    this.mesh.add(meshMain);
+    this.mesh.add(meshTip1);
+    this.mesh.add(meshTip2);
+    this.mesh.add(meshTip3);
 
-        // Mesh positioning
-        // const angle = Math.PI / 2 - Math.PI / 2 * .2;
+    this.mesh.add(tailMesh);
 
-        // this.mesh.position.x = Math.cos(angle) * 100;
-        // this.mesh.position.y = Math.sin(angle) * 100;
-        // this.mesh.rotation.z = angle;
+    this.mesh.lookAt(0, 0, 0);
+    this.mesh.rotation.z = Math.PI / 2;
+    this.mesh.position.z = 250;
 
-        this.mesh.position.z = 60;
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+  }
 
-        this.mesh.castShadow = true;
-        this.mesh.receiveShadow = true;
-    }
+  move(normalizeCoords) {
+    this.position = normalizeCoords;
+  }
 
-    move(normalizeCoords) {
-        this.mesh.position.x = normalize(normalizeCoords[0], -1, 1, -100 + this.mesh.position.z, 100 - this.mesh.position.z);
-        this.mesh.position.y = normalize(normalizeCoords[1], -1, 1, 80, 140);
-    }
-
-    update() {
-
-    }
-
+  update() {
+    this.mesh.position.x = normalize(this.position[0], -1, 1, -100, 100);
+    this.mesh.position.y = normalize(this.position[1], -1, 1, -50, 50);
+  }
 }
 
 function normalize(v, vmin, vmax, tmin, tmax) {
-
-    var nv = Math.max(Math.min(v, vmax), vmin);
-    var dv = vmax - vmin;
-    var pc = (nv - vmin) / dv;
-    var dt = tmax - tmin;
-    var tv = tmin + (pc * dt);
-    return tv;
-
+  var nv = Math.max(Math.min(v, vmax), vmin);
+  var dv = vmax - vmin;
+  var pc = (nv - vmin) / dv;
+  var dt = tmax - tmin;
+  var tv = tmin + (pc * dt);
+  return tv;
 }
